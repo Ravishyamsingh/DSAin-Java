28e19f47d80c3430bcc65d6fbf2b8257ebc67476
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-02-03 23:54:11 +0530

    Add files via upload

---
+++ conditional_stmt.java
@@ -0,0 +1,22 @@
+public class conditional_stmt {
+    public static void main(String[] args) {
+        int salary=25000;
+//        if(salary>10000){
+//            salary=salary+2000;
+//        }else{
+//            salary=salary+1000;
+//        }
+
+        //multiple else if condition
+
+        if(salary>10000){
+            salary=salary+2000;
+        } else if (salary>20000) {
+            salary +=3000;
+        }else{
+
+        salary=salary+1000;
+        }
+        System.out.println(salary);
+    }
+}
