7d5a4ab247e3195264d4f6f142901f3aa59b6f95
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-02-05 22:26:04 +0530

    Add files via upload

---
+++ Switch_main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Switch_main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter fruit name: ");
+        String fruit= in.next();
+
+//        if ( fruit.equals("mango")){
+//            System.out.println("king of fruit ");
+//        }
+//
+//        if (fruit.equals("apple")){
+//            System.out.println("A sweet red fruit");
+//        }
+
+        //Switch case:
+        switch (fruit){
+            case "mango":
+                System.out.println("King of fruit");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+                break;
+            case "Grapes":
+                System.out.println("Small fruit");
+                break; //if breaks is not provided then it will execute all cases
+            default:
+                System.out.println("Please enter a valid fruit ");
+        }
+    }
+}
