1ada54b1bf4bb90c8acd605f54f30f2c98e2ef33
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-03-10 21:27:22 +0530

    Add files via upload

---
+++ CGPACalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CGPACalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = scanner.nextInt();
+
+        double totalGradePoints = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+            System.out.print("Enter grade point for subject " + i + ": ");
+            totalGradePoints += scanner.nextDouble();
+        }
+
+        double cgpa = totalGradePoints / subjects;
+
+        System.out.println("CGPA: " + cgpa);
+
+        scanner.close();
+    }
+}
