6671a67215fedc45bcbac3b598fe694bbaa680fa
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-19 21:32:17 +0530

    Update Arithmatic_operation.java

--- Arithmatic_operation.java
+++ Arithmatic_operation.java
@@ -6,6 +6,7 @@ public class Arithmatic_operation {
         Scanner in = new Scanner(System.in);
         int num1 = in.nextInt();
         int num2 = in.nextInt();
+        
         System.out.println("Enter operator");
         char operator =in.next().charAt(0);
         if (operator=='+'){
