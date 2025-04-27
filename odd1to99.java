84591d89a6035fd8ea0c0f190153b61e63239e23
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-27 20:32:32 +0530

    Update odd1to99.java

--- odd1to99.java
+++ odd1to99.java
@@ -5,6 +5,7 @@ public class odd1to99 {
         // Iterate through numbers from 1 to 99
         for (int i = 1; i < 100; i++) {
             // Check if the number is odd
+            // Check if the first element of the array is 4 or 7 and print the result
             if (i % 2 != 0) {
                 // Print the odd number
                 System.out.println(i);
