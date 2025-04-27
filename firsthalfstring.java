06217d7885bdc1e7bc088ade85e5069ed770e8ba
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-27 20:02:17 +0530

    Update firsthalfstring.java

--- firsthalfstring.java
+++ firsthalfstring.java
@@ -4,7 +4,7 @@ public class firsthalfstring{
     public static void main(String[] args) {
         // Define the main string
         String main_string = "Python";
-
+//System program
         // Extract the substring from the beginning to the middle of the string
         String substring = main_string.substring(0, main_string.length() / 2);
 
