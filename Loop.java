5e5d441f7380735c86097485d9a4ef4d4ed74631
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-17 19:19:14 +0530

    Update Loop.java

--- Loop.java
+++ Loop.java
@@ -17,6 +17,18 @@ public class Loop  {
 
 
 
+                /*
+        for (initialisation; condition;increment/decrement){
+              //body
+        }
+         */
+
+
+        //Q: Print number from 1 to 5
+//        for(int num =1;num<=5;num+=1){
+//            System.out.println(num);
+//        }
+
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 //
