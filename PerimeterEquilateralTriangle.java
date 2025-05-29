087f7892f421e179ff4e2463bcc5e6ad43cfcada
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-29 15:52:45 +0530

    Update PerimeterEquilateralTriangle.java

--- PerimeterEquilateralTriangle.java
+++ PerimeterEquilateralTriangle.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.*;
 public class PerimeterEquilateralTriangle {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
