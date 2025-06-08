debbb2b0bdbd4cddcd73b56aef40e5457583eb9d
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-06-08 22:40:10 +0530

    Update input.java

--- input.java
+++ input.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class input {
+
+
+    
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.print("Please enter roll number: ");
