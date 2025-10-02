d067733513afe032e489ef1595f29cdc33a55df1
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-10-02 22:40:22 +0530

    Update basic.java

--- Array_ArrayList/basic.java
+++ Array_ArrayList/basic.java
@@ -1,18 +1,10 @@
-
-
 package Array_ArrayList;
-
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class basic {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-
-
         //Array of primitive
-
-
         int [] arr=new int[7];
 //        int[] arr=new int[5];
 //        arr[0]=1;
@@ -30,14 +22,10 @@ public class basic {
 //
 //            System.out.print(arr[i] + " ");
 //        }
-
-
         // Array of object
         String[] str=new String[4];
         for (int i = 0; i < str.length; i++) {
             str[i]=sc.next();
-
-
         }
         //srt[1]="ravi";
         System.out.println(Arrays.toString(str));
@@ -45,4 +33,5 @@ public class basic {
         System.out.println(Arrays.toString(str));
     }
 }
+
 
