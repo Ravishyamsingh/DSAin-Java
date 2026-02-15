317c6190df1b5d52dcac0eeb94e784f1c664f0f7
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-02-15 21:17:29 +0530

    Add files via upload

---
+++ Greeting_name.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Greeting_name {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter name:");
+        String name = in.nextLine();
+        System.out.println(name +" Happy morning!");
+    }
+}
