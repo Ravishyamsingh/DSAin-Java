c0e56c1e8a4013806ce341f96b8371cf760a52de
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-02-19 20:43:14 +0530

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -6,18 +7,12 @@ public class Calculator {
 
         while(true){
             System.out.println("Enter the operator: ");
-            
             char op = in.next().trim().charAt(0);
             if(op == '+' || op == '-' ||op == '*' ||op == '/'|| op == '%'){
-                
                 System.out.println("Enter two number: ");
-                
                 int num1=in.nextInt();
-                
                 int num2=in.nextInt();
-                
                 System.out.println();
-                
 
                 if(op == '+'){
                     ans = num1+num2;
@@ -48,5 +43,3 @@ public class Calculator {
 
     }
 }
-
-
